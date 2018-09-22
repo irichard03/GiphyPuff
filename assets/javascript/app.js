c3930089dd1baf2baadb9e66148557cf30bfd80b
Ian Richard <ianrichard03@gmail.com>
2018-09-22 18:47:28 -0500

    fixed bug populating array with empty buttons, fixed add button listener (linked to document).

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,47 @@
 $( document ).ready(function() {
-
-    //declare and initalize variables.
-
+//declare and initalize variables.
     var apiKey = "Y3h4ksc22JmMFoYTKH2XUYmRwrnYL8Gd";
-    var rando = "Romulan";
+    var rando = "Romulan";  
     var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=" + rando +"&key=" + apiKey;
     var starTrek = ["Kirk","Spock","Scotty","Picard","Riker","Sulu","Uhura","Trois","Data","Wharf"];
-
-
-    //Styles the exiting page.
-    setupButtonsInitial();
-
+    var redShirt;
+    var blueShirt;
+    var helper = 0; //if you press button too many times helper will help you out.
     
-    
-    
-     
+//Styles the exiting page.
+    setupButtons();         
+//When any lcars button is clicked.
+    $(document).on('click','.lcarsButton',function(event){
+        event.preventDefault();
+        redShirt = $(this).attr("name");
+        alert(redShirt);                   
+    });
+//When add button is clicked
+    $('#addButton').click(function(event){
+        event.preventDefault();
+        blueShirt = $('#searchBox').val();
+        if(blueShirt){
+            starTrek.push(blueShirt);
+            setupButtons();
+            console.log("Button added successfully:" + blueShirt);
+        }
+        else{
+            console.log("empty press");
+            helper++;
+            if(helper > 1){
+                alert("Use the text box to add a button.")
+                $('#searchBox').css('background-color','yellow');
+            }
+        }
+        
+    });
 
-
-
-    //When any static button is clicked.
-$('.staticButton').click(function(){
-    var redShirt = $(this).attr("name");
-    alert(redShirt);
-    callGiphy(redShirt);    //call giphy API
-});
-    //setup buttonsS
-    function setupButtonsInitial(){
-        for(var i = 0; i< starTrek.length; i++){
-            $(".buttonRow").append(`<button class='staticButton' name='${starTrek[i]}' type='submit'>${starTrek[i]}</button>`);
+//setup buttons
+    function setupButtons(){
+        $('#searchBox').css('background-color','white');
+        $('.buttonRow').empty();
+        for(var i = 0; i < starTrek.length; i++){
+            $('.buttonRow').append(`<button class="lcarsButton" name="${starTrek[i]}" type="submit">${starTrek[i]}</button>`);
         }
     }
 });
