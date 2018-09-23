838c62915c10ea3583b6cf1f47a48d06376a00d7
Ian Richard <ianrichard03@gmail.com>
2018-09-22 21:22:04 -0500

    styled gif output, added onclick to animate gifs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     var apiKey = "Y3h4ksc22JmMFoYTKH2XUYmRwrnYL8Gd";
     var rando = "Romulan";  
     var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=" + rando + "&key=" + apiKey;
-    var starTrek = ["Kirk","Spock","Scotty","Picard","Riker","Sulu","Uhura","Trois","Data","Wharf"];
+    var starTrek = ["Kirk","Spock","Scotty","Picard","Riker","Sulu","Uhura","Trois","Data","Worf"];
     var redShirt;
     var blueShirt;
     var helper = 0; //if you press button too many times helper will help you out.
@@ -26,10 +26,8 @@ $( document ).ready(function() {
                 console.log("api call succeesfull");
                 console.log(response);
                 
-                for (i = 0; i < response.data.length; i++) {
-                    $(".displayZone").append(`<div class='picture${i}'>`)
-                        .append(`<img class='stillGif' src='${response.data[i].images.downsized_still.url}' data-animatedGif='${response.data[i].images.original.url}'>`)
-                        .append(`<p>${response.data[i].rating}</p>`);
+                for (i = 0; i < 20; i++) {
+                    $(".displayZone").append(`<div class="pictureFrame"><img class='stillGif' id='gif${i}' src='${response.data[i].images.downsized_still.url}' data-animatedGif='${response.data[i].images.original.url}'><p>Rated: ${response.data[i].rating}</p></div>`)                
                 }
             }
             else{
@@ -38,13 +36,21 @@ $( document ).ready(function() {
         });
     }
 
-//When any lcars button is clicked.
+//clickevent gif is clicked (should play)
+    $(document).on('click','.stillGif',function(event){
+        event.preventDefault();
+        $(this).attr("src",$(this).attr("data-animatedGif"));
+        console.log(this);
+    });
+    
+
+//clickevent any lcars button is clicked.
     $(document).on('click','.lcarsButton',function(event){
         event.preventDefault();
         redShirt = $(this).attr("name");
         callAPI(redShirt);                   
     });
-//When add button is clicked
+//clickevent add button is clicked
     $('#addButton').click(function(event){
         event.preventDefault();
         blueShirt = $('#searchBox').val().trim();
